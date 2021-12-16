2aa9308e76ea2faed67be9ecbf49ce7e48a7f390
Rain Oselin <rain.oselin@tptlive.ee>
2021-12-16 17:40:24 +0200

    vue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-console.log('hello hdhdhdhd');
+console.log("hallo hdhdhdhd");
 
-import './style.scss';
+import "./style.scss";
+
+import Vue from "vue/dist/vue.common.dev";
+
+new Vue({
+  el: "#app",
+  data: {
+    message: "",
+    items: ["pudru", "või", "jahu"],
+  },
+  computed: {
+    reverseMessage: function () {
+      return this.message.split("").reverse().join("");
+    },
+  },
+  methods: {
+    addItem() {
+        if(this.message.trim() !== ''){
+      this.items.push(this.message);
+      this.message = "";
+    }
+    },
+  },
+});
